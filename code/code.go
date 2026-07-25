2b08c8e7766ad4d6291276c493aef65105b3eaed
diaz <carlosd2mil@gmail.com>
2026-07-24 21:03:32 -0600

    feat(bindings): support for local variables of 1-byte size

--- code/code.go
+++ code/code.go
@@ -18,6 +18,8 @@ const (
 	// Variables
 	OpSetGlobal
 	OpGetGlobal
+	OpSetLocal
+	OpGetLocal
 	OpArray
 	OpHash
 
@@ -65,6 +67,8 @@ var definitions = map[Opcode]*Definition{
 	// Variables
 	OpSetGlobal: {"OpSetGlobal", []int{2}},
 	OpGetGlobal: {"OpGetGlobal", []int{2}},
+	OpSetLocal:  {"OpSetLocal", []int{1}},
+	OpGetLocal:  {"OpGetLocal", []int{1}},
 	OpArray:     {"OpArray", []int{2}},
 	OpHash:      {"OpHash", []int{2}},
 
@@ -126,6 +130,8 @@ func Make(op Opcode, operands ...int) []byte {
 	for i, o := range operands {
 		width := def.OperandWidths[i]
 		switch width {
+		case 1:
+			instruction[offset] = byte(o)
 		case 2:
 			binary.BigEndian.PutUint16(instruction[offset:], uint16(o))
 		}
@@ -183,6 +189,8 @@ func ReadOperands(def *Definition, ins Instructions) ([]int, int) {
 
 	for i, width := range def.OperandWidths {
 		switch width {
+		case 1:
+			operands[i] = int(ReadUint8(ins[offset:]))
 		case 2:
 			operands[i] = int(ReadUint16(ins[offset:]))
 		}
@@ -191,6 +199,10 @@ func ReadOperands(def *Definition, ins Instructions) ([]int, int) {
 	}
 
 	return operands, offset
+}
+
+func ReadUint8(ins Instructions) uint8 {
+	return uint8(ins[0])
 }
 
 func ReadUint16(ins Instructions) uint16 {
