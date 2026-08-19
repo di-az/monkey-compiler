90c7f78c4ffd3db55c945b4045125c8d90f91d90
diaz <carlosd2mil@gmail.com>
2026-08-19 15:29:50 -0600

    feat(recursive-closures): add name to FunctionLiteral struct for identifying self-recursive functions

--- ast/ast.go
+++ ast/ast.go
@@ -2,6 +2,7 @@ package ast
 
 import (
 	"bytes"
+	"fmt"
 	"monkey/token"
 	"strings"
 )
@@ -217,6 +218,7 @@ func (ie *IfExpression) String() string {
 }
 
 type FunctionLiteral struct {
+	Name       string
 	Token      token.Token // The 'fn' token
 	Parameters []*Identifier
 	Body       *BlockStatement
@@ -233,6 +235,9 @@ func (fl *FunctionLiteral) String() string {
 	}
 
 	out.WriteString(fl.TokenLiteral())
+	if fl.Name != "" {
+		out.WriteString(fmt.Sprintf("<%s>", fl.Name))
+	}
 	out.WriteString("(")
 	out.WriteString(strings.Join(params, ", "))
 	out.WriteString(") ")
