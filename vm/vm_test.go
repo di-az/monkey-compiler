b8d91944ec7daed1e479d38f343af419fda70ac0
diaz <carlosd2mil@gmail.com>
2026-08-20 01:25:07 -0600

    feat(recursive-closures): additional test cases for closures

--- vm/vm_test.go
+++ vm/vm_test.go
@@ -760,3 +760,26 @@ func testStringObject(expected string, actual object.Object) error {
 
 	return nil
 }
+
+func TestRecursiveFibonacci(t *testing.T) {
+	tests := []vmTestCase{
+		{
+			input: `
+			let fibonacci = fn(x) {
+				if (x == 0) {
+					return 0;
+				} else {
+					if (x == 1) {
+						return 1;
+					} else {
+						fibonacci(x - 1) + fibonacci(x - 2);
+					}
+				}
+			};
+			fibonacci(15);
+			`,
+			expected: 610,
+		},
+	}
+	runVmTests(t, tests)
+}
